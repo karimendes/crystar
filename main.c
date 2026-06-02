af116fc41c26e0e1df7737bb8096252e656c3f4a
Karine F. Mendes <karineferreiramendes@gmail.com>
2026-06-02 15:37:59 -0300

    Refactor lexer and parser integration in main.c

--- main.c
+++ main.c
@@ -1,45 +1,24 @@
 #include <stdio.h>
 
 #include "token.h"
+#include "ast.h"
 
-// lexer
 void initLexer(const char *filename);
 void closeLexer();
-Token getNextToken();
 
-// parser
-void parseProgram();
+ASTNode* parseProgram();
+
+void printAST(ASTNode* node, int level);
 
 int main() {
 
-    Token token;
-
-    // ANALISE LEXICA 
-
-    printf("===== ANALISE LEXICA =====\n\n");
-
     initLexer("codigo.star");
 
-    do {
+    ASTNode* ast = parseProgram();
 
-        token = getNextToken();
+    printf("\n===== AST =====\n\n");
 
-        printf("Token: %d | Lexema: %s\n",
-               token.type,
-               token.lexeme);
-
-    } while (token.type != TOK_EOF);
-
-    closeLexer();
-
-    // ANALISE SINTATICA 
-
-    printf("\n===== ANALISE SINTATICA =====\n");
-
-    // reinicia lexer
-    initLexer("codigo.star");
-
-    parseProgram();
+    printAST(ast, 0);
 
     closeLexer();
 
