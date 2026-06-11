8c1df9dc0d78c203a101099130593a8d69e2c47f
Karine F. Mendes <karineferreiramendes@gmail.com>
2026-06-11 09:35:10 -0300

    refactor: otimização do main

--- main.c
+++ main.c
@@ -1,34 +1,69 @@
 #include <stdio.h>
-
 #include "ast.h"
 
-// Léxica
+// Lexer
 void initLexer(const char *filename);
 void closeLexer();
 
-// Sintática
+// Parser
 ASTNode* parseProgram();
 
-// Semântica
-void semantic(ASTNode* node);
+// Semantic
+void semantic(ASTNode *root);
+
+void printAST(ASTNode *node, int level) {
+
+    if (node == NULL)
+        return;
+
+    for (int i = 0; i < level; i++)
+        printf("    ");
+
+    printf("%d", node->type);
+
+    if (node->value[0] != '\0')
+        printf(" (%s)", node->value);
+        
+    printf("\n");
+
+    printAST(node->child1, level + 1);
+    printAST(node->child2, level + 1);
+    printAST(node->child3, level + 1);
+    printAST(node->child4, level + 1);
+
+    printAST(node->next, level);
+}
+
+// MAIN
 
 int main() {
 
-    printf("Iniciando compilacao...\n");
+    printf("      COMPILADOR CRYSTAR\n");
 
+    // Inicializa o lexer
     initLexer("codigo.star");
 
-    ASTNode* root = parseProgram();
+    printf("[1] Iniciando analise sintatica...\n");
 
-    printf("Analise sintatica concluida com sucesso!\n");
+    ASTNode *root = parseProgram();
+
+    printf("[OK] Analise sintatica concluida.\n\n");
+
+    printf("             AST\n");
+
+    printAST(root, 0);
+
+    printf("\n");
+
+    printf("      ANALISE SEMANTICA\n");
 
     semantic(root);
 
-    printf("Analise semantica concluida com sucesso!\n");
+    printf("[OK] Analise semantica concluida.\n\n");
 
     closeLexer();
-
-    printf("Compilacao finalizada sem erros.\n");
+    
+    printf(" COMPILACAO FINALIZADA SEM ERROS\n");
 
     return 0;
 }
